e00751932d886c8e1a87da86eee37c2eceba8fbd
HarryPotter1419 <parthdeshmukh5909@gmail.com>
2026-04-21 14:17:34 +0530

    install cloudinary packages

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: 'dvfykxqkg',
-  api_key: '839587931751726',
-  api_secret: 'WvZcIhci-loJC-v-z34vvVzzpqM'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 module.exports = cloudinary;
